05dd6f871f9fc2a361fb6b4102bfaaed1d1ac16b
kasia.kordys <kasia.kordys@gmail.com>
2020-08-03 21:12:51 +0100

    Add project files.

---
+++ EssentialTools/Models/LinqValueCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class LinqValueCalculator
+    {
+        //class will calculate the total price of collection of Product objects
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            return products.Sum(e => e.Price);
+        }
+    }
+}
