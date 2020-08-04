73d8c978c2be214cb000ce1fabf9a8b8e6b490da
kasia.kordys <kasia.kordys@gmail.com>
2020-08-04 15:53:05 +0100

    create the dependency resolver

---
+++ EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using EssentialTools.Models;
+using Ninject;
+
+
+namespace EssentialTools.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        //NinjectDependencyResolver class implements the IDependencyResolver interface
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        //MVC Framework will call the GetService or GetServices methods when it needs an instance of a class to service an incoming request.
+        public object GetService(Type serviceType)
+        {
+
+            //null when there is no suitable binding 
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            //supports multiple bindings for a single type
+            return kernel.GetAll(serviceType);
+        }
+        //binding
+        private void AddBindings()
+        {
+            kernel.Bind<IValueCalculator>().To<LinqValueCalculatorInterface>();
+        }
+    }
+}
