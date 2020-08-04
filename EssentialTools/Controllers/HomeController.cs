14e0db3e6735edcc155d9c6928d5c413a89218a8
kasia.kordys <kasia.kordys@gmail.com>
2020-08-04 20:12:38 +0100

    refactor home controller to use dependency resolver

--- EssentialTools/Controllers/HomeController.cs
+++ EssentialTools/Controllers/HomeController.cs
@@ -6,35 +6,57 @@ namespace EssentialTools.Controllers
 {
     public class HomeController : Controller
     {
-
+        //refactoring home controller to use di
+        private IValueCalculator calc;
         private Product[] products = {
-        new Product {Name = " Kayak", Category = "Watersports", Price = 27.50M},
+        new Product {Name = " Kayak", Category = "Watersports", Price = 37.50M},
         new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
         new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
         new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
 };
-        // GET: Home
+
+        //new home controller and index method, constructor injection
+        //class constructor that accepts implementation of a IValueCaluculator interface changing home controller class so it accepts dependency
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+
         public ActionResult Index()
         {
-            //LinqValueCalculator calc = new LinqValueCalculator();
-
-            //NINJECT 
-            //create an instance of the kernel, object that is resolving dependencies and creating new objects
-            IKernel ninjectKernel = new StandardKernel();
-
-            //bing interface with implementation object
-            //inetrface as a type, clas as a type
-            //dependecies should be resolved by creating an instance of the LinqValueCalculator class
-            ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculatorInterface>();
-
-            //use Get method to create an object
-            IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
 
             ShoppingCartInterface cart = new ShoppingCartInterface(calc) { Products = products };
             decimal totalValue = cart.CalculateProductTotal();
 
             return View(totalValue);
         }
+
+
+
+
+
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //    //LinqValueCalculator calc = new LinqValueCalculator();
+
+        //    //NINJECT 
+        //    //create an instance of the kernel, object that is resolving dependencies and creating new objects
+        //    IKernel ninjectKernel = new StandardKernel();
+
+        //    //bing interface with implementation object
+        //    //inetrface as a type, clas as a type
+        //    //dependecies should be resolved by creating an instance of the LinqValueCalculator class
+        //    ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculatorInterface>();
+
+        //    //use Get method to create an object
+        //    IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
+
+        //    ShoppingCartInterface cart = new ShoppingCartInterface(calc) { Products = products };
+        //    decimal totalValue = cart.CalculateProductTotal();
+
+        //    return View(totalValue);
+        //}
 
         public ActionResult UseInterface()
         {
